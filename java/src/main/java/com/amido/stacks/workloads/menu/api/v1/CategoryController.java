868b338b4f78b7f680831ade78e310391b8af34a
Mohamed Rayan Samandi <msamandi@andigital.com>
2022-07-20 09:24:30 +0200

    Feature/task4747 (#384)
    * task4745-Refactor Swagger annotations to reduce duplication
    
    * fix the format
    
    * fix the format
    
    * fix some comments
    
    * Refactor multiple REST Controller to a single By-Resource REST Controller
    
    * Resolved merge conflict
    
    * Resolved merge conflict
    
    * fix commit delete securtiy annotation
    
    * add steve comments
    
    Co-authored-by: Mohamed Samandi <mohamedsamandi@AML0350.local>

---
+++ java/src/main/java/com/amido/stacks/workloads/menu/api/v1/CategoryController.java
@@ -0,0 +1,107 @@
+package com.amido.stacks.workloads.menu.api.v1;
+
+import static org.springframework.http.HttpStatus.OK;
+
+import com.amido.stacks.core.api.annotations.CreateAPIResponses;
+import com.amido.stacks.core.api.annotations.DeleteAPIResponses;
+import com.amido.stacks.core.api.annotations.UpdateAPIResponses;
+import com.amido.stacks.core.api.dto.response.ResourceCreatedResponse;
+import com.amido.stacks.core.api.dto.response.ResourceUpdatedResponse;
+import com.amido.stacks.workloads.menu.api.v1.dto.request.CreateCategoryRequest;
+import com.amido.stacks.workloads.menu.api.v1.dto.request.UpdateCategoryRequest;
+import com.amido.stacks.workloads.menu.commands.DeleteCategoryCommand;
+import com.amido.stacks.workloads.menu.handlers.CreateCategoryHandler;
+import com.amido.stacks.workloads.menu.handlers.DeleteCategoryHandler;
+import com.amido.stacks.workloads.menu.handlers.UpdateCategoryHandler;
+import com.amido.stacks.workloads.menu.mappers.RequestToCommandMapper;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.Parameter;
+import java.util.UUID;
+import javax.validation.Valid;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestAttribute;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping(path = "/v1/menu/{id}/category")
+public class CategoryController {
+
+  private final CreateCategoryHandler createCategoryHandler;
+  private final UpdateCategoryHandler updateCategoryHandler;
+  private final DeleteCategoryHandler deleteCategoryHandler;
+  private final RequestToCommandMapper requestToCommandMapper;
+
+  public CategoryController(
+      CreateCategoryHandler createCategoryHandler,
+      RequestToCommandMapper requestToCommandMapper,
+      UpdateCategoryHandler updateCategoryHandler,
+      DeleteCategoryHandler deleteCategoryHandler) {
+    this.createCategoryHandler = createCategoryHandler;
+    this.requestToCommandMapper = requestToCommandMapper;
+    this.updateCategoryHandler = updateCategoryHandler;
+    this.deleteCategoryHandler = deleteCategoryHandler;
+  }
+
+  @PostMapping
+  @Operation(
+      tags = "Category",
+      summary = "Create a category in the menu",
+      description = "Adds a category to menu",
+      operationId = "AddMenuCategory")
+  @CreateAPIResponses
+  ResponseEntity<ResourceCreatedResponse> addMenuCategory(
+      @Parameter(description = "Menu id", required = true) @PathVariable("id") UUID menuId,
+      @Valid @RequestBody CreateCategoryRequest body,
+      @Parameter(hidden = true) @RequestAttribute("CorrelationId") String correlationId) {
+    return new ResponseEntity<>(
+        new ResourceCreatedResponse(
+            createCategoryHandler
+                .handle(requestToCommandMapper.map(correlationId, menuId, body))
+                .orElseThrow()),
+        HttpStatus.CREATED);
+  }
+
+  @PutMapping(value = "/{categoryId}")
+  @Operation(
+      tags = "Category",
+      summary = "Update a category in the menu",
+      description = "Update a category to menu",
+      operationId = "UpdateMenuCategory")
+  @UpdateAPIResponses
+  ResponseEntity<ResourceUpdatedResponse> updateMenuCategory(
+      @Parameter(description = "Menu id", required = true) @PathVariable("id") UUID menuId,
+      @Parameter(description = "Category id", required = true) @PathVariable("categoryId")
+          UUID categoryId,
+      @Valid @RequestBody UpdateCategoryRequest body,
+      @Parameter(hidden = true) @RequestAttribute("CorrelationId") String correlationId) {
+    return new ResponseEntity<>(
+        new ResourceUpdatedResponse(
+            updateCategoryHandler
+                .handle(requestToCommandMapper.map(correlationId, menuId, categoryId, body))
+                .orElseThrow()),
+        OK);
+  }
+
+  @DeleteMapping(value = "/{categoryId}")
+  @Operation(
+      tags = "Category",
+      summary = "Removes a category and its items from menu",
+      description = "Removes a category and its items from menu",
+      operationId = "DeleteCategory")
+  @DeleteAPIResponses
+  ResponseEntity<Void> deleteCategory(
+      @Parameter(description = "Menu id", required = true) @PathVariable("id") UUID menuId,
+      @Parameter(description = "Category id", required = true) @PathVariable("categoryId")
+          UUID categoryId,
+      @Parameter(hidden = true) @RequestAttribute("CorrelationId") String correlationId) {
+    deleteCategoryHandler.handle(new DeleteCategoryCommand(correlationId, menuId, categoryId));
+    return new ResponseEntity<>(OK);
+  }
+}
--- java/src/test/java/com/amido/stacks/workloads/menu/api/v1/CategoryControllerTest.java
+++ java/src/test/java/com/amido/stacks/workloads/menu/api/v1/CategoryControllerTest.java
@@ -1,23 +1,28 @@
-package com.amido.stacks.workloads.menu.api.v1.impl;
+package com.amido.stacks.workloads.menu.api.v1;
 
 import static com.amido.stacks.workloads.menu.domain.CategoryHelper.createCategories;
 import static com.amido.stacks.workloads.menu.domain.CategoryHelper.createCategory;
+import static com.amido.stacks.workloads.menu.domain.ItemHelper.createItem;
 import static com.amido.stacks.workloads.menu.domain.MenuHelper.createMenu;
 import static com.amido.stacks.workloads.util.TestHelper.getBaseURL;
 import static com.amido.stacks.workloads.util.TestHelper.getRequestHttpEntity;
 import static com.azure.cosmos.implementation.Utils.randomUUID;
 import static java.util.UUID.fromString;
 import static org.assertj.core.api.BDDAssertions.then;
+import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.ArgumentMatchers.eq;
 import static org.mockito.Mockito.times;
 import static org.mockito.Mockito.verify;
 import static org.mockito.Mockito.when;
+import static org.springframework.http.HttpStatus.BAD_REQUEST;
 import static org.springframework.http.HttpStatus.CONFLICT;
 import static org.springframework.http.HttpStatus.NOT_FOUND;
 import static org.springframework.http.HttpStatus.OK;
 
 import com.amido.stacks.core.api.dto.ErrorResponse;
+import com.amido.stacks.core.api.dto.response.ResourceCreatedResponse;
 import com.amido.stacks.core.api.dto.response.ResourceUpdatedResponse;
+import com.amido.stacks.workloads.menu.api.v1.dto.request.CreateCategoryRequest;
 import com.amido.stacks.workloads.menu.api.v1.dto.request.UpdateCategoryRequest;
 import com.amido.stacks.workloads.menu.domain.Category;
 import com.amido.stacks.workloads.menu.domain.Menu;
@@ -25,6 +30,7 @@ import com.amido.stacks.workloads.menu.repository.MenuRepository;
 import com.azure.spring.autoconfigure.cosmos.CosmosAutoConfiguration;
 import com.azure.spring.autoconfigure.cosmos.CosmosHealthConfiguration;
 import com.azure.spring.autoconfigure.cosmos.CosmosRepositoriesAutoConfiguration;
+import java.util.ArrayList;
 import java.util.List;
 import java.util.Optional;
 import java.util.UUID;
@@ -51,15 +57,151 @@ import org.springframework.test.context.ActiveProfiles;
     })
 @Tag("Integration")
 @ActiveProfiles("test")
-class UpdateCategoryControllerImplTest {
+class CategoryControllerTest {
 
+  public static final String CREATE_CATEGORY = "%s/v1/menu/%s/category";
   public static final String UPDATE_CATEGORY = "%s/v1/menu/%s/category/%s";
+  public static final String DELETE_CATEGORY = "%s/v1/menu/%s/category/%s";
 
   @LocalServerPort private int port;
 
   @Autowired private TestRestTemplate testRestTemplate;
 
   @MockBean private MenuRepository menuRepository;
+
+  @Test
+  void testCanNotAddCategoryIfMenuNotPresent() {
+    // Given
+    UUID menuId = randomUUID();
+    when(menuRepository.findById(eq(menuId.toString()))).thenReturn(Optional.empty());
+
+    CreateCategoryRequest request =
+        new CreateCategoryRequest("test Category Name", "test Category Description");
+
+    // When
+    var response =
+        this.testRestTemplate.postForEntity(
+            String.format(CREATE_CATEGORY, getBaseURL(port), menuId), request, ErrorResponse.class);
+
+    // Then
+    then(response).isNotNull();
+    then(response.getStatusCode()).isEqualTo(HttpStatus.NOT_FOUND);
+  }
+
+  @Test
+  void testNoDescriptionGivenReturnsBadRequest() {
+    // Given
+    CreateCategoryRequest request = new CreateCategoryRequest("test Category Name", "");
+
+    // When
+    var response =
+        this.testRestTemplate.postForEntity(
+            String.format(CREATE_CATEGORY, getBaseURL(port), randomUUID()),
+            request,
+            ErrorResponse.class);
+
+    // Then
+    then(response).isNotNull();
+    then(response.getStatusCode()).isEqualTo(BAD_REQUEST);
+    then(response.getBody().getDescription())
+        .isEqualTo("Invalid Request: {description=must not be blank}");
+  }
+
+  @Test
+  void testInvalidMenuIdWilThrowBadRequest() {
+    // Given
+    CreateCategoryRequest request = new CreateCategoryRequest("test Category Name", "");
+
+    // When
+    var response =
+        this.testRestTemplate.postForEntity(
+            String.format(CREATE_CATEGORY, getBaseURL(port), "XXXXXX"),
+            request,
+            ErrorResponse.class);
+
+    // Then
+    then(response).isNotNull();
+    then(response.getStatusCode()).isEqualTo(BAD_REQUEST);
+  }
+
+  @Test
+  void testAddCategory() {
+    // Given
+    Menu menu = createMenu(1);
+    when(menuRepository.findById(eq(menu.getId()))).thenReturn(Optional.of(menu));
+    when(menuRepository.save(any(Menu.class))).thenReturn(menu);
+
+    CreateCategoryRequest request =
+        new CreateCategoryRequest("test Category Name", "test Category Description");
+
+    // When
+    var response =
+        this.testRestTemplate.postForEntity(
+            String.format(CREATE_CATEGORY, getBaseURL(port), menu.getId()),
+            request,
+            ResourceCreatedResponse.class);
+
+    // Then
+    ArgumentCaptor<Menu> captor = ArgumentCaptor.forClass(Menu.class);
+    verify(menuRepository, times(1)).save(captor.capture());
+    Menu created = captor.getValue();
+
+    then(created.getName()).isEqualTo(menu.getName());
+    then(created.getDescription()).isEqualTo(menu.getDescription());
+    then(created.getCategories().size()).isEqualTo(1);
+    then(created.getCategories().get(0).getName()).isEqualTo(request.getName());
+    then(created.getCategories().get(0).getDescription()).isEqualTo(request.getDescription());
+
+    then(response).isNotNull();
+    then(response.getBody()).isNotNull();
+    then(response.getStatusCode()).isEqualTo(HttpStatus.CREATED);
+    then(created.getCategories().get(0).getId()).isEqualTo(response.getBody().getId().toString());
+  }
+
+  @Test
+  void testCannotAddCategoryWhichAlreadyExists() {
+    // Given
+    Menu menu = createMenu(1);
+    Category category =
+        new Category(
+            UUID.randomUUID().toString(), "cat name", "cat description", new ArrayList<>());
+    menu.addOrUpdateCategory(category);
+
+    when(menuRepository.findById(eq(menu.getId()))).thenReturn(Optional.of(menu));
+
+    CreateCategoryRequest request =
+        new CreateCategoryRequest(category.getName(), "test Category Description");
+
+    // When
+    var response =
+        this.testRestTemplate.postForEntity(
+            String.format(CREATE_CATEGORY, getBaseURL(port), menu.getId()),
+            request,
+            ErrorResponse.class);
+
+    // Then
+    then(response).isNotNull();
+    then(response.getStatusCode()).isEqualTo(HttpStatus.CONFLICT);
+  }
+
+  @Test
+  void testNoNameGivenReturnsBadRequest() {
+    // Given
+    CreateCategoryRequest request = new CreateCategoryRequest("", "Some description");
+
+    // When
+    var response =
+        this.testRestTemplate.postForEntity(
+            String.format(CREATE_CATEGORY, getBaseURL(port), randomUUID()),
+            request,
+            ErrorResponse.class);
+
+    // Then
+    then(response).isNotNull();
+    then(response.getStatusCode()).isEqualTo(BAD_REQUEST);
+    then(response.getBody().getDescription())
+        .isEqualTo("Invalid Request: {name=must not be blank}");
+  }
 
   @Test
   void testUpdateCategorySuccess() {
@@ -310,5 +452,108 @@ class UpdateCategoryControllerImplTest {
     then(response.getStatusCode()).isEqualTo(HttpStatus.BAD_REQUEST);
     then(response.getBody().getDescription())
         .isEqualTo("Invalid Request: {description=must not be blank}");
+  }
+
+  @Test
+  void testDeleteCategorySuccess() {
+    // Given
+    Menu menu = createMenu(1);
+    Category category = createCategory(0);
+    menu.setCategories(List.of(category));
+    when(menuRepository.findById(eq(menu.getId()))).thenReturn(Optional.of(menu));
+
+    // When
+    String requestUrl =
+        String.format(DELETE_CATEGORY, getBaseURL(port), menu.getId(), category.getId());
+    var response =
+        this.testRestTemplate.exchange(
+            requestUrl,
+            HttpMethod.DELETE,
+            new HttpEntity<>(getRequestHttpEntity()),
+            ErrorResponse.class);
+
+    // Then
+    verify(menuRepository, times(1)).save(menu);
+    then(response.getStatusCode()).isEqualTo(OK);
+    Optional<Menu> optMenu = menuRepository.findById(menu.getId());
+    Menu updated = optMenu.get();
+    then(updated.getCategories()).isNotNull();
+  }
+
+  @Test
+  void testDeleteCategoryWithAnItem() {
+    // Given
+    Menu menu = createMenu(1);
+    Category category = createCategory(0);
+    category.addOrUpdateItem(createItem(0));
+    menu.addOrUpdateCategory(category);
+
+    when(menuRepository.findById(eq(menu.getId()))).thenReturn(Optional.of(menu));
+
+    // When
+    String requestUrl =
+        String.format(DELETE_CATEGORY, getBaseURL(port), menu.getId(), category.getId());
+    var response =
+        this.testRestTemplate.exchange(
+            requestUrl,
+            HttpMethod.DELETE,
+            new HttpEntity<>(getRequestHttpEntity()),
+            ErrorResponse.class);
+
+    // Then
+    then(response.getStatusCode()).isEqualTo(OK);
+    ArgumentCaptor<Menu> captor = ArgumentCaptor.forClass(Menu.class);
+    verify(menuRepository, times(1)).save(captor.capture());
+    Menu updatedMenu = captor.getValue();
+    then(updatedMenu.getCategories()).isNotNull();
+  }
+
+  @Test
+  void testDeleteCategoryWithInvalidCategoryId() {
+    // Given
+    Menu menu = createMenu(1);
+    Category category = createCategory(0);
+    menu.setCategories(List.of(category));
+    when(menuRepository.findById(eq(menu.getId()))).thenReturn(Optional.of(menu));
+
+    // When
+    String requestUrl =
+        String.format(DELETE_CATEGORY, getBaseURL(port), menu.getId(), menu.getId());
+    var response =
+        this.testRestTemplate.exchange(
+            requestUrl,
+            HttpMethod.DELETE,
+            new HttpEntity<>(getRequestHttpEntity()),
+            ErrorResponse.class);
+
+    // Then
+    verify(menuRepository, times(0)).save(menu);
+    then(response.getStatusCode()).isEqualTo(NOT_FOUND);
+  }
+
+  @Test
+  void testDeleteACategoryFromList() {
+    // Given
+    Menu menu = createMenu(1);
+    List<Category> categories = createCategories(2);
+    menu.setCategories(categories);
+    when(menuRepository.findById(eq(menu.getId()))).thenReturn(Optional.of(menu));
+
+    // When
+    String requestUrl =
+        String.format(DELETE_CATEGORY, getBaseURL(port), menu.getId(), categories.get(0).getId());
+    var response =
+        this.testRestTemplate.exchange(
+            requestUrl,
+            HttpMethod.DELETE,
+            new HttpEntity<>(getRequestHttpEntity()),
+            ErrorResponse.class);
+
+    // Then
+    verify(menuRepository, times(1)).save(menu);
+    then(response.getStatusCode()).isEqualTo(OK);
+    Optional<Menu> byId = menuRepository.findById(menu.getId());
+    Menu updatedMenu = byId.get();
+    then(updatedMenu.getCategories()).hasSize(1);
   }
 }
