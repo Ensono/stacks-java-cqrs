4c7ce952348dac545985cab9c0f88b4556f78c21
Jack Blower <Jack@elvenspellmaker.co.uk>
2024-08-20 12:32:40 +0100

    fix: fix

--- deploy/azure/app/kube/main.tf
+++ deploy/azure/app/kube/main.tf
@@ -7,7 +7,7 @@ data "azurerm_client_config" "current" {}
 
 # Naming convention
 module "default_label" {
-  source     = "git::https://github.com/cloudposse/terraform-null-label.git?ref=0.24.1"
+  source     = "git::https://github.com/cloudposse/terraform-null-label.git?ref=0.25.0"
   namespace  = "${var.name_company}-${var.name_project}"
   stage      = var.name_environment
   name       = "${lookup(var.location_name_map, var.resource_group_location, "uksouth")}-${var.name_domain}"
