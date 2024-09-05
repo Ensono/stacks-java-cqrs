ccb1e46ce421833537cfc1f4bc0a716005d9151a
Jack Blower <Jack@elvenspellmaker.co.uk>
2024-09-05 19:34:34 +0100

    fix: fix

--- java/src/main/java/com/amido/stacks/workloads/ApplicationConfig.java
+++ java/src/main/java/com/amido/stacks/workloads/ApplicationConfig.java
@@ -4,13 +4,13 @@ import com.fasterxml.jackson.databind.DeserializationFeature;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import java.util.Arrays;
 import java.util.List;
-// import org.apache.commons.lang3.BooleanUtils;
+import org.apache.commons.lang3.BooleanUtils;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.core.Ordered;
 import org.springframework.core.annotation.Order;
-// import org.springframework.security.config.Customizer;
+import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.web.SecurityFilterChain;
@@ -78,10 +78,11 @@ public class ApplicationConfig {
    */
   @Bean
   public SecurityFilterChain configure(HttpSecurity http) throws Exception {
-    // http.cors(Customizer.withDefaults());
-    // if (BooleanUtils.isTrue(isEnabled)) {
-    //   return enableAuth(http);
-    // }
+    http.cors(Customizer.withDefaults());
+    if (BooleanUtils.isTrue(isEnabled)) {
+      return enableAuth(http);
+    }
+
     return permitAll(http);
   }
 
